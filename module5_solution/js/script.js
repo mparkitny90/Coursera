8eb64491e5439895ff36133f5c60508eefc9041d
mparkitn <mparkitn>
2016-07-11 23:33:03 +0200

    5

--- module5_solution/js/script.js
+++ module5_solution/js/script.js
@@ -118,7 +118,7 @@ function buildAndShowHomeHTML (categories) {
       // console.log(chosenCategoryShortName);
       var specials = "" + chosenCategoryShortName.short_name; 
       var homeHtmlToInsertIntoMainPage = insertProperty(homeHtmlUrl, "randomCategoryShortName", specials);
-      // console.log(homeHtmlToInsertIntoMainPage);
+      console.log(homeHtmlToInsertIntoMainPage);
       
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
